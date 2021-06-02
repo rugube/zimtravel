86b7649fc230fd069ac0b9ff859afa12a1df37d6
Ronald Rugube <rrugube69@gmail.com>
2021-06-02 16:22:47 +0200

    Added icons to screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            Container(
+                height: 57.6,
+                margin: EdgeInsets.only(top: 28.8, left: 28.8, right: 28.8),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 57.6,
+                      width: 57.6,
+                      padding: EdgeInsets.all(18),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(9.6),
+                          color: Color(0x080a0928)),
+                      child: SvgPicture.asset('assets/svg/icon_drawer.svg'),
+                    ),
+                  ],
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
